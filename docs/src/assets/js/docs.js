cc89374f6e032dde1e062f9147c5466d31385129
liwy <liwenyao_java@126.com>
2017-08-30 21:01:50 +0800

    modify docs

--- docs/src/assets/js/docs.js
+++ docs/src/assets/js/docs.js
@@ -1,3 +1,29 @@
-function add(a,b) {
-	return a+b;
-}
+!function($){
+	// Scrollspy
+    var $window = $(window)
+    var $body   = $(document.body)
+
+    $body.scrollspy({
+      target: '.liwy-sidebar'
+    })
+    $window.on('load', function () {
+      $body.scrollspy('refresh')
+    })
+
+    // Sidenav affixing
+    var $sideBar = $('.liwy-sidebar')
+  	$sideBar.affix({
+  		offset: {
+  			top: function () {
+            	var offsetTop      = $sideBar.offset().top;
+            	var sideBarMargin  = parseInt($sideBar.children(0).css('margin-top'), 10);
+            	var navOuterHeight = $('header').height()+20;
+            	console.log(offsetTop);
+            	console.log(sideBarMargin);
+            	console.log(navOuterHeight);
+            	return (this.top = offsetTop)
+          	}
+  		}
+  	});
+
+}(jQuery)
